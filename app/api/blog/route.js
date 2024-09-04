21cf4819d0131217bb16bbad9342750b14ac2838
Robeeen <robeeen@gmail.com>
2024-09-04 23:51:58 +0600

    API develop

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -20,7 +20,7 @@ export async function POST(request){
 
     const image = formData.get('image');
     const imageByteData = await image.arrayBuffer();
-    const buffer = buffer.from(imageByteData);
+    const buffer = Buffer.from(imageByteData);
     const path = `./public/${timeStamp}_${image.name}`;
 
     await writeFile(path, buffer);
@@ -28,6 +28,18 @@ export async function POST(request){
 
     //test
     console.log(imgUrl);
+
+    const blogData = {
+        title: `${formData.get('title')}`,
+        description: `${formData.get('description')}`,
+        category: `${formData.get('category')}`,
+        author: `${formData.get('author')}`,
+        image: `${imgUrl}`,
+        authorImg: `${formData.get('authorImg')}`
+    }
+
+
+
     return NextResponse.json({imgUrl});
 
 }
