cc9d21cc94e22ecbe058b01991f5761bba8f255a
Robeeen <robeeen@gmail.com>
2024-09-05 00:22:25 +0600

    API successfully done

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,5 +1,6 @@
 import { writeFile } from "fs/promises";
 import { ConnectDB } from "@/lib/config/db";
+import BlogModel from "@/lib/models/BlogModel"
 
 const { NextResponse } = require("next/server")
 
@@ -39,7 +40,7 @@ export async function POST(request){
     }
 
     await BlogModel.create(blogData);
-
+    console.log("Blog Saved.");
 
 
 
