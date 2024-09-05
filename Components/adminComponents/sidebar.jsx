51f1c9509898d0cfbaaf9dd2f92619215b37dfb9
Robeeen <robeeen@gmail.com>
2024-09-05 22:36:40 +0600

    Admin section

--- Components/adminComponents/sidebar.jsx
+++ Components/adminComponents/sidebar.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { assets } from '../../Assets/assets'
+import Image from 'next/image'
 
-const sidebar = () => {
+
+const Sidebar = () => {
   return (
-    <div className='flex flex-col #bg-slate-100'>
-        <div className='px-2 sm:pl-14 py-3 border #border-black'>
+    <div className='flex flex-col bg-slate-100'>
+        <div className='px-2 sm:pl-14 py-3 border border-black'>
             <Image src={assets.logo} width={120} alt='' />
         </div>
+        <div>Add Content</div>
 
     </div>
   )
 }
 
-export default sidebar
+export default Sidebar 
--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,6 +1,12 @@
+import Sidebar from "@/Components/adminComponents/Sidebar"
 export default function Layout({children}){
     return (
         <>
+
+        <div className="flex">
+
+           <Sidebar />
+        </div>
             {children}
         </>
     )
