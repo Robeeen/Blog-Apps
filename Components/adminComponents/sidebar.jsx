5a6c91c9adffe34186f8f91a9d6569e360971e44
Robeeen <robeeen@gmail.com>
2024-09-05 23:11:01 +0600

    Admin section

--- Components/adminComponents/sidebar.jsx
+++ Components/adminComponents/sidebar.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import { assets } from '../../Assets/assets'
 import Image from 'next/image'
+import Link from 'next/link'
 
 
 const Sidebar = () => {
-  return (
-    <div className='flex flex-col bg-slate-100'>
-        <div className='px-2 sm:pl-14 py-3 border border-black'>
-            <Image src={assets.logo} width={120} alt='' />
-        </div>
-        <div>Add Content</div>
+    return (
+        <div className='flex flex-col bg-slate-100'>
+            <div className='px-2 sm:pl-14 py-3 border border-black'>
+                <Image src={assets.logo} width={120} alt='' />
+            </div>
 
-    </div>
-  )
+            <div className='w-28 sm:w-80 h-[100vh] relative py-12 border border-black' >
+                <div className='w-[50%] sm:w-[80%] absolute right-0'>
+                    <Link href='/admin/addProduct' className='flex items-center border broder-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]'>
+                        <Image src={assets.add_icon} width={28} alt='' /><p>Add Blog</p>
+                    </Link>
+                </div>
+
+            </div>
+
+
+
+        </div>
+    )
 }
 
 export default Sidebar 
