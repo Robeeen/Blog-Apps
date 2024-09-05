42c2df1190cd0377002eac1ea2913fc2d198984b
Robeeen <robeeen@gmail.com>
2024-09-05 17:41:08 +0600

    admin panel clreate

---
+++ Components/adminComponents/sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const sidebar = () => {
+  return (
+    <div className='flex flex-col #bg-slate-100'>
+        <div className='px-2 sm:pl-14 py-3 border #border-black'>
+            <Image src={assets.logo} width={120} alt='' />
+        </div>
+
+    </div>
+  )
+}
+
+export default sidebar
