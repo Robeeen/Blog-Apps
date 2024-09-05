f78571e619d81f260173b389fa0f0914079e390e
Robeeen <robeeen@gmail.com>
2024-09-05 23:45:35 +0600

    Admin section

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -4,8 +4,13 @@ export default function Layout({children}){
         <>
 
         <div className="flex">
-
            <Sidebar />
+           <div className="flex flex-col w-full">
+            <div className="flex items-center justigy-between w-full py-3 max-h-[60px] px-12 border-b border-black ">
+
+            </div>
+
+           </div>
         </div>
             {children}
         </>
