835bf1dcd9d14db6fd9a8636e7ab695e03a7c0f8
Robeeen <robeeen@gmail.com>
2024-09-06 11:17:02 +0600

    Admin Panel - add blog form states

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -7,9 +7,18 @@ const page = () => {
 
   const [ image, setImage ] = useState(false);
 
+  const [ data, setData ] = useState({
+      title: '',
+      description: '',
+      category: 'Startup',
+      author: 'Alex',
+      authorImg: '/author_img.png'
+  });
+
   return (
     <>
       <form className='pt-5 px-5 sm:pt-12 sm:pl-16'>
+        
         <p className='text-xl'>Upload Thumbnail</p>
         <label htmlFor='image'>
           <Image src={!image?assets.upload_area:URL.createObjectURL(image)} className='mt-4' width={140} height={70} />
@@ -21,12 +30,16 @@ const page = () => {
 
         <p className='text-xl mt-7'>Blog Description</p>
         <textarea className='w-full sm:w-[500] mt-4 px-4 py-3 border' type='text' rows={6} placeholder='Blog Description' required />
+        
         <p className='text-xl mt-7'>Blog Category</p>
         <select name='category' className='w-40 mt-4 px-4 py-3 border text-grey-500'>
           <option value="Startup">Startup</option>
           <option value="Technology">Technology</option>
           <option value="LifeStype">LifeStype</option>
         </select>
+        <br />
+
+        <button type='submit' className='mt-8 w-40 h-12 bg-black text-white'>Add Blog</button>
 
       </form>
     </>
