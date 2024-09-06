a0dfd00dee13d2b7dbec0536970000a86545e3c2
Robeeen <robeeen@gmail.com>
2024-09-06 09:44:10 +0600

    Admin Panel Dashboard

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -15,8 +15,18 @@ const page = () => {
           <Image src={!image?assets.upload_area:URL.createObjectURL(image)} className='mt-4' width={140} height={70} />
         </label>
         <input onChange={(e) => setImage(e.target.files[0])} type="file" id='image'  hidden required/>
+        
         <p className='text-xl mt-7'>Blog Title</p>
         <input className='w-full sm:w-[500] mt-4 px-4 py-3 border' type='text' placeholder='Blog title' required />
+
+        <p className='text-xl mt-7'>Blog Description</p>
+        <textarea className='w-full sm:w-[500] mt-4 px-4 py-3 border' type='text' rows={6} placeholder='Blog Description' required />
+        <p className='text-xl mt-7'>Blog Category</p>
+        <select name='category' className='w-40 mt-4 px-4 py-3 border text-grey-500'>
+          <option value="Startup">Startup</option>
+          <option value="Technology">Technology</option>
+          <option value="LifeStype">LifeStype</option>
+        </select>
 
       </form>
     </>
