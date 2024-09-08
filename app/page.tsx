24b237f3c37b625fd4e93a6f3fdcf1e01955cb5e
Robeeen <robeeen@gmail.com>
2024-09-08 21:49:21 +0600

    Creating front-end blog list and single blog

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import SingleBlog from "@/Components/front-end/SingleBlog"
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return (<>
+  <h1>Hello, Next.js!</h1>  
+  <SingleBlog />
+  
+  
+  </>)
+  
+  
+  
 }
