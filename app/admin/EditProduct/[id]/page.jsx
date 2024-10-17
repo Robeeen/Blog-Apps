1a9a08985442e196fd52a6b591d2279f70eed13c
Robeeen <robeeen@gmail.com>
2024-10-17 16:59:52 +0600

    Built new api to edit blog and get single blog

--- app/admin/EditProduct/[id]/page.jsx
+++ app/admin/EditProduct/[id]/page.jsx
@@ -4,3 +4,20 @@ import { toast } from 'react-toastify'
 import axios from 'axios'
 import { useParams } from 'next/navigation'
 
+const page = () =>{
+    const [ blogs, setBlogs ] = useState({});
+    const {id} = useParams('_id');
+
+    //To fetch and display before edit
+    const fetchBlogs = async () => {
+        const response = await axios.get(`/api/blog/${id}`);
+        setBlogs(response.data.blogs);    
+    }
+
+    useEffect(() => {
+        fetchBlogs
+    }, []);
+
+
+}
+
