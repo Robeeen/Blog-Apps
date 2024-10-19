de889abd089955609cdb60051a106f97da059f48
Robeeen <robeeen@gmail.com>
2024-10-19 23:49:29 +0600

    display single blog page

--- blogs/[id]/page.jsx
+++ blogs/[id]/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { assets, blog_data } from '@/Assets/assets';
 import axios from 'axios';
 
@@ -9,6 +9,22 @@ const page = ({params}) => {
     const [ data, setData ] = useState(null);
 
     const fetchBlogData = async () => {
-        const response = await axios.get()
+        const response = await axios.get('/api/blog' .{
+            params: {
+                id: params.id
+            }
+        })
+        setData(response.data);
     }
+
+    useEffect(() => {
+        fetchBlogData();
+    });
+
+    return(data?
+    <>
+    
+    </>: ''
+
+    )
 }
